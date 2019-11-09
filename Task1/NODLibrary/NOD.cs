501fdae39079fb548573deef1c108b05c2249d5d
AlexisGonchar <alexgonchar00@gmail.com>
2019-11-09 18:15:06 +0300

    created a method with the implementation of the Euclidean algorithm for computing the GCD

---
+++ Task1/NODLibrary/NOD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NODLibrary
+{
+    public static class NOD
+    {
+        public static int findGcdEuclid(int a, int b)
+        {
+            while(a != 0 && b != 0)
+            {
+                if(a > b)
+                {
+                    a = a % b;
+                }
+                else
+                {
+                    b = b % a;
+                }
+            }
+
+            return (a + b);
+        }
+    }
+}
---
+++ Task1/UnitTestTask1/UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NODLibrary;
+
+namespace UnitTestTask1
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 30,
+                b = 18,
+                expGcd = 6,
+                actualGcd = NOD.findGcdEuclid(a, b);
+            Assert.AreEqual(expGcd, actualGcd);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 1071,
+                b = 462,
+                expGcd = 21,
+                actualGcd = NOD.findGcdEuclid(a, b);
+            Assert.AreEqual(expGcd, actualGcd);
+        }
+    }
+}
