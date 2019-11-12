b217115d074764e7908c620fa0da6cbabb18f5c2
AlexisGonchar <alexgonchar00@gmail.com>
2019-11-12 14:46:02 +0300

    created a three dimensional vector class

---
+++ Task2/MathLibrary/Vector3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class Vector3
+    {
+        public float X { get; protected set; }
+        public float Y { get; protected set; }
+        public float Z { get; protected set; }
+
+        public Vector3(float X, float Y, float Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+    }
+}
