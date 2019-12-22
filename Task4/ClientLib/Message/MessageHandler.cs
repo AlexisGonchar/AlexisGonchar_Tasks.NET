762bde2ebd7f35ae4cc51319ca3951e6833a775b
AlexisGonchar <alexgonchar00@gmail.com>
2019-12-22 17:44:10 +0300

    Added events classes

---
+++ Task4/ClientLib/Message/MessageHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientLib
+{
+    /// <summary>
+    /// Delegate describing the method of processing the text of the message.
+    /// </summary>
+    /// <returns></returns>
+    public delegate string ReceiveMsg();
+
+    /// <summary>
+    /// Class for processing messages.
+    /// </summary>
+    public class ClientMessageHandler
+    {
+        /// <summary>
+        /// Message Receive Event.
+        /// </summary>
+        public event ReceiveMsg OnLoadMsg;
+    }
+}
