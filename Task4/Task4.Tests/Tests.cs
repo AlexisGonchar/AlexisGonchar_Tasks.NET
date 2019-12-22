23dc33268c0e4e195029dfe656978de97f719057
AlexisGonchar <alexgonchar00@gmail.com>
2019-12-23 01:58:52 +0300

    added test for collections messages

--- Task4/Task4.Tests/Tests.cs
+++ Task4/Task4.Tests/Tests.cs
@@ -107,5 +107,51 @@ namespace Task4.Tests
             Assert.NotNull(actString);
             Assert.AreEqual(expString, actString);
         }
+
+        /// <summary>
+        /// Testing Response from Server
+        /// </summary>
+        [Test]
+        public void TestCollection()
+        {
+            bool waitClientReceive = true;
+            int count = 0;
+
+            Action runClient = () =>
+            {
+                Client client = new Client("Alexis");
+                ClientSocket clientSocket = new ClientSocket(client);
+                Message message = new Message("Hello User", client);
+                clientSocket.Connect();
+                clientSocket.Send(message);
+                while (waitClientReceive) ;
+                clientSocket.Receive();
+                clientSocket.Close();
+            };
+
+            Action runServer = () =>
+            {
+                Server server = new Server(5, 8080, "127.0.0.1");
+                try
+                {
+                    server.WaitClientConnection();
+                }
+                catch { }
+                Message messages = server.Receive();
+                waitClientReceive = false;
+                server.Send(messages);
+                count = server.GetMessages().Count;
+            };
+
+            Thread serverThread = new Thread(new ThreadStart(runServer));
+            Thread clientThread = new Thread(new ThreadStart(runClient));
+
+            serverThread.Start();
+            clientThread.Start();
+
+            Thread.Sleep(2000);
+            
+            Assert.AreEqual(count, 1);
+        }
     }
 }
