94d910a407c4fcac6cb566153e9ef010cdd57800
AlexisGonchar <alexgonchar00@gmail.com>
2020-01-29 22:51:52 +0300

    Create solution Task7, created methods for read and update groups table

---
+++ Task7/Task7.Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UniversityDAO;
+using UniversityORM;
+
+namespace Task7.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private static string connString = "Server=localhost;Database=universitydb;port=3306;User Id=root";
+        string connectionString = "Persist Security Info=False;Integrated Security=SSPI;database= universitydb;server= localhost;Connect Timeout=30";
+        [Test]
+        public void Test()
+        {
+            GroupDao groupDao = new GroupDao(connString);
+            Group group = groupDao.Read(1);
+            Assert.AreEqual("ИТ-11", group.Name);
+        }
+
+        [Test]
+        public void TestUpdate()
+        {
+            GroupDao groupDao = new GroupDao(connString);
+            Group group = new Group { Name = "ИТИ" };
+            groupDao.Update(group, 1);
+        }
+    }
+}
---
+++ Task7/UniversityORM/Group.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Linq.Mapping;
+
+namespace UniversityORM
+{
+    /// <summary>
+    /// Group class
+    /// </summary>
+    [Table (Name = "Groups")]
+    public class Group
+    {
+        /// <summary>
+        /// Group identification number.
+        /// </summary>
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Group name.
+        /// </summary>
+        [Column(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
